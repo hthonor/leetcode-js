5315cee655fb08be109f7a5233181efa155ed832
Joyling <cgl85by@126.com>
2016-03-11 13:02:35 +0800

    add
    add

---
+++ easy/228SummaryRanges.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @return {string[]}
+ */
+var summaryRanges = function(nums) {
+    var string=[];
+    if (nums.length!==0){
+        for(var i=0;i<nums.length;){
+        var j=i;
+        var start = nums[i];
+        var end = nums[j];
+        while((nums[++j]==++start&&(j<nums.length))){
+            end=nums[j];
+        }
+        if(nums[i]==end){
+            string.push(end);
+             
+        }else{
+            string.push(nums[i]+'->'+end);
+
+        }
+        i=j;
+    }
+    }else{
+        string=[];
+    }
+    
+    return string;
+    };
